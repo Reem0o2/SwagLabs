7d131461a09d7f1373fdfa43ba91b86ac36a0365
Reem0o2 <reem.emara309@gmail.com>
2025-12-23 11:16:31 +0200

    commit FireFoxDriverObject

---
+++ SwagLabs/src/test/java/driver_factory/FireFoxDriverObject.java
@@ -0,0 +1,18 @@
+package driver_factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FireFoxDriverObject {
+    public static WebDriver getFireFoxDriver(){
+        WebDriver driver=new FirefoxDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
